1d37cb254d3fbeaba77a8997b036a50d0dd2fe0b
clue cat <gitlab@clue.cat>
2025-06-27 20:28:12 -0400

    add revealed pickup tilesets for sector 5

--- src/nonlinear/tileset-edits.s
+++ src/nonlinear/tileset-edits.s
@@ -38,6 +38,14 @@
     tilemap @Tileset08Tilemap, "data/tilemaps/08-maindeck-hallways.bin", 900h
 
 
+; Tileset 0B - BSL Grated Hallways
+.org TilesetEntry + (TilesetEntry_Size * 0Bh) + TilesetEntry_RlebgGfxPointer
+    tilesetgfx  @Tileset0BRlebgGfx, "data/tilesets/tileset-0B.gfx", 1BE9h
+
+.org TilesetEntry + (TilesetEntry_Size * 0Bh) + TilesetEntry_TilemapPointer
+    tilemap @Tileset0BTilemap, "data/tilemaps/0B-bsl-grated-hallways.bin", 0E04h
+
+
 ; Tileset 0D - Recharge Rooms
 .org readptr(TilesetEntry + (TilesetEntry_Size * 0Dh) + TilesetEntry_TilemapPointer)
 .area 3588
@@ -61,7 +69,7 @@
     tilesetgfx  @Tileset1BRlebgGfx, "data/tilesets/tileset-1B.gfx", 1BB5h
 
 .org TilesetEntry + (TilesetEntry_Size * 1Bh) + TilesetEntry_TilemapPointer
-    tilemap @Tileset1BTilemap, "data/tilemaps/1B-maindeck-maintenance-tunnels.bin", 0D80h
+    tilemap @Tileset1BTilemap, "data/tilemaps/1B-maindeck-maintenance-tunnels.bin", 0D84h
 
 
 ; Tileset 20 - Sector 2 Lush Greenery
@@ -96,7 +104,7 @@
     tilemap @Tileset29Tilemap, "data/tilemaps/29-srx-terrain.bin", 1084h
 
 
-; Tileset 2F - Sector 4 Upper
+; Tileset 2B & 2F - Sector 4 Upper
 .org TilesetEntry + (TilesetEntry_Size * 2Bh) + TilesetEntry_RlebgGfxPointer
     tilesetgfx  @Tileset2B2FRlebgGfx, "data/tilesets/tileset-2B-2F.gfx", 2656h
 
@@ -113,6 +121,22 @@
 .endarea
 
 
+; Tileset 30 - Sector 5 Non-frozen rooms
+.org TilesetEntry + (TilesetEntry_Size * 30h) + TilesetEntry_RlebgGfxPointer
+    tilesetgfx  @Tileset30RlebgGfx, "data/tilesets/tileset-30.gfx", 34FFh
+
+.org TilesetEntry + (TilesetEntry_Size * 30h) + TilesetEntry_TilemapPointer
+    tilemap @Tileset30Tilemap, "data/tilemaps/30-arc-normal.bin", 1484h
+
+
+; Tileset 31 - Sector 5 Frozen rooms
+.org TilesetEntry + (TilesetEntry_Size * 31h) + TilesetEntry_RlebgGfxPointer
+    tilesetgfx  @Tileset31RlebgGfx, "data/tilesets/tileset-31.gfx", 2523h
+
+.org TilesetEntry + (TilesetEntry_Size * 31h) + TilesetEntry_TilemapPointer
+    tilemap @Tileset31Tilemap, "data/tilemaps/31-arc-frozen.bin", 1004h
+
+
 ; Tileset 34 - Sector 4 Lower
 .org TilesetEntry + (TilesetEntry_Size * 34h) + TilesetEntry_RlebgGfxPointer
     tilesetgfx  @Tileset34RlebgGfx, "data/tilesets/tileset-34.gfx", 2FF9h
@@ -121,12 +145,21 @@
     tilemap @Tileset34Tilemap, "data/tilemaps/34-aqa-lower.bin", 1304h
 
 
-; Tileset 43 - Generic Blue Rooms
+; Tileset 43 & 44 - Generic Blue Rooms
 .org TilesetEntry + (TilesetEntry_Size * 43h) + TilesetEntry_RlebgGfxPointer
-    tilesetgfx  @Tileset43RlebgGfx, "data/tilesets/tileset-43.gfx", 0DF2h
+    tilesetgfx  @Tileset4344RlebgGfx, "data/tilesets/tileset-43-44.gfx", 0DF2h
 
 .org TilesetEntry + (TilesetEntry_Size * 43h) + TilesetEntry_TilemapPointer
-    tilemap @Tileset43Tilemap, "data/tilemaps/43-blue-rooms.bin", 0E04h
+    tilemap @Tileset4344Tilemap, "data/tilemaps/43-44-blue-rooms.bin", 0E04h
+
+.org TilesetEntry + (TilesetEntry_Size * 44h) + TilesetEntry_RlebgGfxPointer
+.area 4
+    .dw     @Tileset4344RlebgGfx
+.endarea
+.org TilesetEntry + (TilesetEntry_Size * 44h) + TilesetEntry_TilemapPointer
+.area 4
+    .dw     @Tileset4344Tilemap
+.endarea
 
 
 ; Tileset 54 - Main Deck Restricted Lab Glass Tubes
