aabf7f12655817628cde8216326a178298f38fd9
biosp4rk <37962487+biosp4rk@users.noreply.github.com>
2025-07-05 13:54:10 -0700

    Navigation hint graphics

--- src/randomizer/hints.s
+++ src/randomizer/hints.s
@@ -140,3 +140,204 @@ HintSecurityLevels:
     .db     0Bh
     .db     00h
     .skip 3
+
+
+; Navigation hint graphics
+
+.autoregion
+
+SpawnNavLockSprite:
+    push    r14
+    add     sp, #-0Ch
+    ; Spawn lock sprite
+    ldr     r1, =CurrentSprite
+    ldrh    r0, [r1, #2]
+    sub     r0, #0F0h
+    str     r0, [sp]        ; Y position
+    ldrh    r0, [r1, #4]
+    str     r0, [sp, #4]    ; X position
+    mov     r0, #0
+    str     r0, [sp, #8]    ; Status
+    mov     r0, NavLockSpriteId
+    mov     r1, #0          ; Room slot
+    mov     r2, NavLockGfxRow
+    mov     r3, #20h        ; Property | spriteset slot
+    bl      SpawnPrimarySprite
+    ; Load graphics and palette
+    mov     r4, #0
+@@LoadGfxLoop:
+    mov     r0, NavLockSpriteId
+    mov     r1, NavLockGfxRow
+    mov     r2, r4
+    bl      LoadNewSpriteGfx
+    add     r4, 1
+    cmp     r4, NavLockGfxRowCount * 8
+    bcc     @@LoadGfxLoop
+    mov     r0, NavLockSpriteId
+    mov     r1, NavLockGfxRow
+    mov     r2, NavLockGfxRowCount
+    bl      LoadNewSpritePal
+    ; Overwritten code
+    ldr     r5, =CurrentSprite
+    mov     r2, r5
+    ; Return
+    add     sp, #0Ch
+    pop     r15
+
+NavLockAi:
+    push    r4
+    ldr     r3, =CurrentSprite
+    mov     r4, r3
+    add     r4, Sprite_Pose
+    ; Check pose
+    ldrb    r0, [r4]
+    cmp     r0, #0
+    beq     @@PoseInit
+    cmp     r0, #1
+    beq     @@PoseIdle
+    b       @@Return
+@@PoseInit:
+    ; Get hint level for navigation room
+    ldr     r2, =CurrentNavRoom
+    ldrb    r0, [r2]
+    ldr     r2, =HintSecurityLevels
+    add     r2, r2, r0
+    ldrb    r0, [r2]
+    cmp     r0, #4
+    bhi     @@RemoveAndReturn
+    ; Get OAM address based on hint level
+    ldr     r2, =NavLockOamPtrs
+    lsl     r0, r0, #2
+    add     r2, r2, r0
+    ldr     r0, [r2]
+    str     r0, [r3, Sprite_OamPointer]
+    mov     r0, #0
+    strb    r0, [r3, Sprite_AnimationCounter]
+    strh    r0, [r3, Sprite_AnimationFrame]
+    ; Update status
+    ldrh    r0, [r3, Sprite_Status]
+    mov     r1, #80h
+    lsl     r1, r1, #8
+    orr     r0, r1          ; Set "Ignore projectiles"
+    mov     r1, SpriteStatus_NotDrawn
+    mvn     r1, r1
+    and     r0, r1          ; Remove "Not drawn"
+    strh    r0, [r3, Sprite_Status]
+    ; Set property
+    mov     r2, r3
+    add     r2, Sprite_Properties
+    ldrb    r0, [r2]
+    mov     r1, 1 << SpriteProps_NoCollision
+    orr     r0, r1
+    strb    r0, [r2]
+    ; Set Samus property
+    mov     r2, r3
+    add     r2, Sprite_SamusCollision
+    mov     r0, #0
+    strb    r0, [r2]
+    ; Set draw order
+    mov     r2, r3
+    add     r2, Sprite_DrawOrder
+    mov     r0, #0Dh
+    strb    r0, [r2]
+    ; Set draw boundaries
+    mov     r2, r3
+    add     r2, Sprite_DrawDistanceTop
+    mov     r0, #16
+    strb    r0, [r2]
+    add     r2, Sprite_DrawDistanceBottom - Sprite_DrawDistanceTop
+    strb    r0, [r2]
+    mov     r0, #32
+    add     r2, Sprite_DrawDistanceHorizontal - Sprite_DrawDistanceBottom
+    strb    r0, [r2]
+    ; Set hitbox
+    mov     r0, #4
+    strh    r0, [r3, Sprite_BboxBottom]
+    strh    r0, [r3, Sprite_BboxRight]
+    mvn     r0, r0
+    strh    r0, [r3, Sprite_BboxTop]
+    strh    r0, [r3, Sprite_BboxLeft]
+    ; Set pose to 1
+    mov     r0, #1
+    strb    r0, [r4]
+    b       @@Return
+@@RemoveAndReturn:
+    mov     r0, #0
+    strh    r0, [r3, Sprite_Status]
+    b       @@Return
+@@PoseIdle:
+    ; Nothing to do for now
+@@Return:
+    pop     r4
+    bx      r14
+    .pool
+
+; New data
+
+.align 4
+NavLockGfx:
+    .incbin "data/nav-lock/nav-lock.gfx"
+NavLockGfx_End:
+
+.align 4
+NavLockPal:
+    .incbin "data/nav-lock/nav-lock-pal.bin"
+
+.align 2
+NavLockOam_L0_Frame0:
+    nav_lock_oam_frame 0
+NavLockOam_L1_Frame0:
+    nav_lock_oam_frame 1
+NavLockOam_L2_Frame0:
+    nav_lock_oam_frame 2
+NavLockOam_L3_Frame0:
+    nav_lock_oam_frame 3
+NavLockOam_L4_Frame0:
+    nav_lock_oam_frame 4
+
+.align 4
+NavLockOam_L0:
+    .dw NavLockOam_L0_Frame0, 0FFh
+    .dw 0, 0
+NavLockOam_L1:
+    .dw NavLockOam_L1_Frame0, 0FFh
+    .dw 0, 0
+NavLockOam_L2:
+    .dw NavLockOam_L2_Frame0, 0FFh
+    .dw 0, 0
+NavLockOam_L3:
+    .dw NavLockOam_L3_Frame0, 0FFh
+    .dw 0, 0
+NavLockOam_L4:
+    .dw NavLockOam_L4_Frame0, 0FFh
+    .dw 0, 0
+
+.align 4
+NavLockOamPtrs:
+    .dw NavLockOam_L0
+    .dw NavLockOam_L1
+    .dw NavLockOam_L2
+    .dw NavLockOam_L3
+    .dw NavLockOam_L4
+
+.endautoregion
+
+; Modified code
+
+; Hijack NavigationPad_Init to spawn nav lock sprite
+.org 0802A120h
+    bl      SpawnNavLockSprite
+
+; Modified data
+
+.org SpriteGfxLengths + ((NavLockSpriteId - 10h) * 4)
+    .dw NavLockGfx_End - NavLockGfx
+
+.org PrimarySpriteAiPtrs + (NavLockSpriteId * 4)
+    .dw NavLockAi + 1
+
+.org SpriteGfxPtrs + ((NavLockSpriteId - 10h) * 4)
+    .dw NavLockGfx
+
+.org SpritePalettePtrs + ((NavLockSpriteId - 10h) * 4)
+    .dw NavLockPal
