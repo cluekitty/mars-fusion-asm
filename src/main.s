73bf5438ab75e9e5b1fcec1e98b3b2eadde30af8
clue cat <gitlab@clue.cat>
2025-07-08 15:37:59 -0400

    fix inconsistent underwater bomb jumps fixes #271

--- src/main.s
+++ src/main.s
@@ -170,6 +170,7 @@ DataFreeSpaceEnd equ DataFreeSpace + DataFreeSpaceLen
 .include "src/nonlinear/null-event.s"
 .include "src/nonlinear/operations-room.s"
 .include "src/nonlinear/security-unlock.s"
+.include "src/physics/bomb-jump.s"
 .include "src/physics/single-walljump.s"
 .include "src/nonlinear/split-suits.s"
 .include "src/nonlinear/story-flags.s"
---
+++ src/physics/bomb-jump.s
@@ -0,0 +1,5 @@
+; Removes check for iframes when bomb jumping
+.org 080856AAh
+.area 080856B2h - 080856AAh, 0
+    b       080856B2h
+.endarea
