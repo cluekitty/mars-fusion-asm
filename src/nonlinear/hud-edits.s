7b0a79eaf54fb17d6be8337f0662fb953f492d4d
clue cat <gitlab@clue.cat>
2025-08-24 11:07:29 -0400

    clean up some comments for cleanliness and clarity

--- src/nonlinear/hud-edits.s
+++ src/nonlinear/hud-edits.s
@@ -43,7 +43,7 @@
     mov     r1, #0
     strb    r1, [r0]
 
-; Clear "Thousands" digit if health is decreasing
+    ; Clear "Thousands" digit if health is decreasing
     ldr     r0, =06010E0Eh  ; Center Line of Tile
     ldr     r0, [r0]
     mov     r1, #00h
@@ -112,13 +112,12 @@
     str     r2, [r6, DMA_CNT]
     ldr     r2, [r6, DMA_CNT]
 
-
+; separate and load digits into RAM
 @@max_thousands:
     ldr     r5, =MaxEnergyDigits
     mov     r0, r9
     mov     r1, #1000 >> 2
     lsl     r1, #02h
-    ; r0 / r1
     bl      DivideUnsigned
     lsl     r0, #18h
     lsr     r0, #18h
@@ -138,7 +137,6 @@
 @@max_hundreds:
     mov     r0, r9
     mov     r1, #100
-    ; r0 / r1
     bl      DivideUnsigned
     lsl     r0, #10h
     lsr     r0, #10h
@@ -158,7 +156,6 @@
 @@max_tens:
     mov     r0, r9
     mov     r1, #10
-    ; r0 / r1
     bl      DivideUnsigned
     lsl     r0, #10h
     lsr     r0, #10h
@@ -178,7 +175,6 @@
 @@max_ones:
     mov     r0, r9
     mov     r1, #10
-    ; r0 % r1
     bl      ModuloUnsigned
     lsl     r0, #18h
     lsr     r0, #18h
@@ -191,78 +187,78 @@
 @@set_max_ones_digit:
     strb    r0, [r5, EnergyDigits_Ones]
 
-@@load_max_gfx:
+    ; Load "Max" graphics pointers
     ldr     r7, =AmmoDigitsGfx+AmmoDigitsGfx_Alt
     ldr     r6, =MaxEnergyDigitsGfxWRAM
 
-    ; 1st loop
-    mov     r2, #00h    ; row
-    mov     r3, r2      ; pixels
+    ; Draw digits of max health, 1st loop
+    mov     r2, #00h    ; Tile Row
+    mov     r3, r2      ; VRAM Offset
     ldrb    r4, [r5, EnergyDigits_Thousands]
     ldrb    r5, [r5, EnergyDigits_Hundreds]
     lsl     r4, #15h
     lsr     r4, #10h
     lsl     r5, #15h
     lsr     r5, #10h
-
-
 /*
     This draws the graphics one row at a time to a temporary spot in IWRAM, then
     copies it into VRAM using DMA. This is effectively how vanilla draws ammo
     digits.
 */
 @@loop_left_digits:
-@@max_thousands_gfx:
-    add     r0, r4, r7  ; src = thousands + gfx
-    add     r1, r3, r6  ; dest = pixels + wram
-    ldrb    r0, [r0]
-    strb    r0, [r1]
-    add     r3, #01h    ; increment pixels
+bkpt
+    ; Max Health Thousands Digit
+    ; The comments for this section also apply to the other digits
+    add     r0, r4, r7  ; source graphics offset = digit offset + gfx ptr
+    add     r1, r3, r6  ; dest = tile offset + wram
+    ldrb    r0, [r0]    ; load gfx tile offset from ROM
+    strb    r0, [r1]    ; store gfx tile offset in RAM
+    add     r3, #01h    ; increment tile offset
     lsl     r3, #18h
-    lsr     r3, #18h    ; Ensure we only have a byte
-    add     r4, #01h    ; increment thousands
+    lsr     r3, #18h    ; Ensure we only have a byte (make this clearer)
+    add     r4, #01h    ; increment digit rom gfx offset
     lsl     r4, #10h
-    lsr     r4, #10h    ; ensure we have hword
-    add     r0, r4, r7  ; src = thousands + gfx
-    add     r1, r3, r6  ; dest = pixels + wram
+    lsr     r4, #10h    ; ensure we have hword (make this clearer)
+    add     r0, r4, r7  ; src = digit + gfx
+    add     r1, r3, r6  ; dest = tile offset + wram
     ldrb    r0, [r0]
     strb    r0, [r1]
-    add     r3, #01h    ; increment pixels
+    add     r3, #01h    ; increment tile offset
     lsl     r3, #18h
     lsr     r3, #18h    ; Ensure we only have a byte
-    add     r4, #03h    ; increment thousands
+    add     r4, #03h    ; increment thousands (make this clearer why it's by 3)
     lsl     r4, #10h
     lsr     r4, #10h    ; ensure we have hword
 
-@@max_hundreds_gfx:
-    add     r0, r5, r7  ; src = hundreds + gfx
-    add     r1, r3, r6  ; dest = pixels + wram
+    ; Max Health Hundreds Digit
+    add     r0, r5, r7
+    add     r1, r3, r6
     ldrb    r0, [r0]
     strb    r0, [r1]
-    add     r3, #01h    ; increment pixels
+    add     r3, #01h
     lsl     r3, #18h
-    lsr     r3, #18h    ; Ensure we only have a byte
-    add     r5, #01h    ; increment hundreds
+    lsr     r3, #18h
+    add     r5, #01h
     lsl     r5, #10h
-    lsr     r5, #10h    ; ensure we have hword
-    add     r0, r5, r7  ; src = hundreds + gfx
-    add     r1, r3, r6  ; dest = pixels + wram
+    lsr     r5, #10h
+    add     r0, r5, r7
+    add     r1, r3, r6
     ldrb    r0, [r0]
     strb    r0, [r1]
-    add     r3, #01h    ; increment pixels
+    add     r3, #01h
     lsl     r3, #18h
-    lsr     r3, #18h    ; Ensure we only have a byte
-    add     r5, #03h    ; increment hundreds
+    lsr     r3, #18h
+    add     r5, #03h
     lsl     r5, #10h
-    lsr     r5, #10h    ; ensure we have hword
+    lsr     r5, #10h
 
-    add     r2, #01h    ; increment row
+    add     r2, #01h
     cmp     r2, #07h
     bls     @@loop_left_digits
 
     ; 2nd loop
-    mov     r2, #00h    ; row
-    mov     r3, #20h    ; pixels
+    mov     r2, #00h
+    mov     r3, #(GFX_TILE * 2)
     ldr     r5, =MaxEnergyDigits
     ldrb    r4, [r5, EnergyDigits_Tens]
     ldrb    r5, [r5, EnergyDigits_Ones]
@@ -270,61 +266,60 @@
     lsr     r4, #10h
     lsl     r5, #15h
     lsr     r5, #10h
-    b       @@loop_right_digits
 
 @@loop_right_digits:
-@@max_tens_gfx:
-    add     r0, r4, r7  ; src = tens + gfx
-    add     r1, r3, r6  ; dest = pixels + wram
+    ; Max Health Tens Digit
+    add     r0, r4, r7
+    add     r1, r3, r6
     ldrb    r0, [r0]
     strb    r0, [r1]
-    add     r3, #01h    ; increment pixels
+    add     r3, #01h
     lsl     r3, #18h
-    lsr     r3, #18h    ; Ensure we only have a byte
-    add     r4, #01h    ; increment tens
+    lsr     r3, #18h
+    add     r4, #01h
     lsl     r4, #10h
-    lsr     r4, #10h    ; ensure we have hword
-    add     r0, r4, r7  ; src = tens + gfx
-    add     r1, r3, r6  ; dest = pixels + wram
+    lsr     r4, #10h
+    add     r0, r4, r7
+    add     r1, r3, r6
     ldrb    r0, [r0]
     strb    r0, [r1]
-    add     r3, #01h    ; increment pixels
+    add     r3, #01h
     lsl     r3, #18h
-    lsr     r3, #18h    ; Ensure we only have a byte
-    add     r4, #03h    ; increment tens
+    lsr     r3, #18h
+    add     r4, #03h
     lsl     r4, #10h
-    lsr     r4, #10h    ; ensure we have hword
+    lsr     r4, #10h
 
-@@max_ones_gfx:
-    add     r0, r5, r7  ; src = ones + gfx
-    add     r1, r3, r6  ; dest = pixels + wram
+    ; Max Health Ones Digit
+    add     r0, r5, r7
+    add     r1, r3, r6
     ldrb    r0, [r0]
     strb    r0, [r1]
-    add     r3, #01h    ; increment pixels
+    add     r3, #01h
     lsl     r3, #18h
-    lsr     r3, #18h    ; Ensure we only have a byte
-    add     r5, #01h    ; increment ones
+    lsr     r3, #18h
+    add     r5, #01h
     lsl     r5, #10h
-    lsr     r5, #10h    ; ensure we have hword
-    add     r0, r5, r7  ; src = ones + gfx
-    add     r1, r3, r6  ; dest = pixels + wram
+    lsr     r5, #10h
+    add     r0, r5, r7
+    add     r1, r3, r6
     ldrb    r0, [r0]
     strb    r0, [r1]
-    add     r3, #01h    ; increment pixels
+    add     r3, #01h
     lsl     r3, #18h
-    lsr     r3, #18h    ; Ensure we only have a byte
-    add     r5, #03h    ; increment ones
+    lsr     r3, #18h
+    add     r5, #03h
     lsl     r5, #10h
-    lsr     r5, #10h    ; ensure we have hword
+    lsr     r5, #10h
 
-    add     r2, #01h    ; increment row
+    add     r2, #01h
     cmp     r2, #07h
     bls     @@loop_right_digits
 
-    mov     r2, #01h
-    mov     r3, #44h
+    ; draws a static line to the right of max energy digits
+    mov     r2, #01h                ; Tile Row
+    mov     r3, #(GFX_TILE * 2) + 4 ; VRAM Offset
 
-; draws a static line to the right of max energy digits
 @@ending_line:
     mov     r0, #0Eh
     add     r1, r3, r6
@@ -337,12 +332,9 @@
     cmp     r2, #07h
     bls     @@ending_line
 
-
     ldr     r6, =DMA3
     ldr     r0, =MaxEnergyDigitsGfxWRAM
-    ;ldr     r1, =06010E00h ; Directly below "Energy" text
-    ;ldr     r1, =06010A60h ; Directly next to "Energy" Text
-    ldr     r1, =06010AA0h ; 3 tiles right of "Energy" Text, need to make gx for "Max" text
+    ldr     r1, =06010AA0h ; Directly to the right of "Energy"
     ldr     r2, =DMA_ENABLE | 30h
     str     r0, [r6, DMA_SAD]
     str     r1, [r6, DMA_DAD]
@@ -377,13 +369,12 @@
 @@check_curr_thousands_digit_is_same:
     ldrb    r1, [r5, EnergyDigits_Thousands]
     cmp     r0, r1
-    beq     @@curr_hundreds     ; if thousands digit doesn't change, skip DMA
+    beq     @@curr_hundreds ; if thousands digit doesn't change, skip DMA
     strb    r0, [r5, EnergyDigits_Thousands]
 @@load_curr_thousands_gfx:
-    lsl     r0, #05h ; r0 * 20h (length of 8x8 tile)
+    lsl     r0, #05h        ; r0 * 20h (length of 8x8 tile)
     add     r0, r7
     ldr     r1, =06010E00h
-    ;ldr     r1, =06010E80h
     ldr     r2, =DMA_ENABLE | 10h
     str     r0, [r6, DMA_SAD]
     str     r1, [r6, DMA_DAD]
@@ -410,13 +401,12 @@
 @@check_curr_hundreds_digit_is_same:
     ldrb    r1, [r5, EnergyDigits_Hundreds]
     cmp     r0, r1
-    beq     @@curr_tens    ; if hundreds digit doesn't change, skip DMA
+    beq     @@curr_tens     ; if hundreds digit doesn't change, skip DMA
     strb    r0, [r5, EnergyDigits_Hundreds]
 @@load_curr_hundreds_gfx:
-    lsl     r0, #05h ; r0 * 20h (length of 8x8 tile)
+    lsl     r0, #05h        ; r0 * 20h (length of 8x8 tile)
     add     r0, r7
     ldr     r1, =06010E20h
-    ;ldr     r1, =06010EA0h
     ldr     r2, =DMA_ENABLE | 10h
     str     r0, [r6, DMA_SAD]
     str     r1, [r6, DMA_DAD]
@@ -443,13 +433,12 @@
 @@check_curr_tens_digit_is_same:
     ldrb    r1, [r5, EnergyDigits_Tens]
     cmp     r0, r1
-    beq     @@curr_ones    ; if tens digit doesn't change, skip DMA
+    beq     @@curr_ones     ; if tens digit doesn't change, skip DMA
     strb    r0, [r5, EnergyDigits_Tens]
 @@load_curr_tens_gfx:
-    lsl     r0, #05h ; r0 * 20h (length of 8x8 tile)
+    lsl     r0, #05h        ; r0 * 20h (length of 8x8 tile)
     add     r0, r7
     ldr     r1, =06010E40h
-    ;ldr     r1, =06010EC0h
     ldr     r2, =DMA_ENABLE | 10h
     str     r0, [r6, DMA_SAD]
     str     r1, [r6, DMA_DAD]
@@ -475,10 +464,9 @@
     beq     @@return    ; if ones digit doesn't change, skip DMA
     strb    r0, [r5, EnergyDigits_Ones]
 @@load_curr_ones_gfx:
-    lsl     r0, #05h ; r0 * 20h (length of 8x8 tile)
+    lsl     r0, #05h    ; r0 * 20h (length of 8x8 tile)
     add     r0, r7
     ldr     r1, =06010E60h
-    ;ldr     r1, =06010EE0h
     ldr     r2, =DMA_ENABLE | 10h
     str     r0, [r6, DMA_SAD]
     str     r1, [r6, DMA_DAD]
