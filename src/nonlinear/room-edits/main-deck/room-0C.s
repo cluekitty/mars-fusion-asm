8f631f8efb758c441cbd2d2ef78e519e69456b03
clue cat <gitlab@clue.cat>
2025-08-25 12:03:53 -0400

    Rename file to correct room id for room MD-0C

--- src/nonlinear/room-edits/main-deck/room-0C.s
+++ src/nonlinear/room-edits/main-deck/room-0C.s
@@ -1,6 +1,5 @@
-; Main Deck - Crew Quarters East
+; Main Deck - Crew Quarters West
 ; remove power bomb geron
-; Slightly adjust exit-distance from door with gadora to prevent instant damage if the gadora is not defeated.
 
 .defineregion readptr(MainDeckLevels + 0Ch * LevelMeta_Size + LevelMeta_Spriteset1), 0Fh
 
@@ -14,9 +13,4 @@
     .skip 2
     .dw     NullSpriteset
     .db     0
-.endarea
-
-.org MainDeckDoors + 56h * DoorEntry_Size + DoorEntry_ExitDistanceX
-.area 1
-    .db     100h - 23h
 .endarea
