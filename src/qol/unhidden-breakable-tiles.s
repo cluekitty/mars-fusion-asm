0cd8252fce09b806261a5084f965d83b94153a07
clue cat <gitlab@clue.cat>
2025-09-27 22:27:53 -0400

    reduce overall number of memory read/writes and multiply usage

--- src/qol/unhidden-breakable-tiles.s
+++ src/qol/unhidden-breakable-tiles.s
@@ -102,7 +102,14 @@ RevealHiddenTilesFlag:
 .autoregion
     .align 2
 .func RevealHiddenBreakableTiles
+; Max Cycles 157618 in crumble city
+;            157769
+; r8-r11 are basically unused, we can speed up by storing temp vars here instead of on the stack
     push    { r4-r7, lr }
+    mov     r4, r8
+    mov     r5, r9
+    mov     r6, r10
+    push    { r4-r5 }
     sub     sp, #8
     ldr     r0, =RevealHiddenTilesFlag
     ldrb    r0, [r0]
@@ -120,35 +127,41 @@ RevealHiddenTilesFlag:
     ldr     r2, =StoredRevealedTiles
     ldr     r3, =StoredRevealedTiles_Len * StoredRevealedTiles_Size
     bl      BitFill
-    b       @@load_room_info
-    .pool
 @@load_room_info:
     ldr     r7, =LevelLayers + LevelLayers_Clipdata
     ldrh    r6, [r7, LevelLayer_Rows]   ; height
     ldrh    r5, [r7, LevelLayer_Stride] ; width
     ldr     r7, [r7]
-    str     r5, [sp]
-    str     r6, [sp, #4]
-    mov     r5, #0
-    mov     r6, #0
+    mov     r2, r5
+    mul     r2, r6
+    add     r2, r2, r5
+    lsl     r2, #01
+    mov     r10, r2                     ; end of room
+    mov     r8, r5                      ; width [sp]
+    mov     r9, r6                     ; height [sp+4]
+;    mov     r5, #0
+;    mov     r6, #0
 @@loop:
     mov     r1, r7
-    ldr     r3, [sp]
+    mov     r3, r8
     mov     r2, r6
-    mul     r2, r3      ; height * room width
-    add     r2, r2, r5  ; + width
-    lsl     r2, #1
+    ;mul     r2, r3      ; height * room width
+    ;add     r2, r2, r5  ; + width
+    ;lsl     r2, #1
+    mov     r2, r10
     add     r0, r1, r2
-    ldr     r1, =3001h
-    cmp     r1, r2      ; don't read past Clipdata RAM
+    ;ldr     r1, =3001h
+    cmp     r2, #0      ; don't read past Clipdata RAM
     beq     @@return
     ldrh    r0, [r7, r2]
-    cmp     r0, #ClipdataTile_Air
-    beq     @@inc_width
-    cmp     r0, #ClipdataTile_Solid
-    beq     @@inc_width
-    cmp     r0, #ClipdataTile_DoorTransition
-    beq     @@inc_width
+    ;cmp     r0, #ClipdataTile_Air
+    ;beq     @@dec_width
+    ;cmp     r0, #ClipdataTile_Solid
+    ;beq     @@dec_width
+    ;cmp     r0, #ClipdataTile_DoorTransition
+    ;beq     @@dec_width
+    cmp     r0, #ClipdataTile_2x2TopLeftNeverReform
+    bls     @@dec_width
     bl      @SearchForHiddenBlocks
     mov     r1, #0
     mvn     r1, r1
@@ -156,26 +169,33 @@ RevealHiddenTilesFlag:
     lsr     r1, #10h
     cmp     r1, r0  ; if not FFFF
     bne     @@reveal_hidden_block
-    b       @@inc_width
+    b       @@dec_width
 @@reveal_hidden_block:
     ; r0 should contain value of new tile
     mov     r1, r6  ; Y Pos
     mov     r2, r5  ; X Pos
     bl      SetSpecialBg1Tile
-@@inc_width:
-    add     r5, #1
-    ldr     r0, [sp]
-    cmp     r5, r0
-    ble     @@loop
-@@inc_height:
-    add     r6, #1
-    ldr     r0, [sp, #4]
-    cmp     r6, r0
+@@dec_width:
+    mov     r2, r10
+    sub     r2, #02
+    mov     r10, r2
+    sub     r5, #01
+    ;mov     r0, r8
+    cmp     r5, #00
+    bgt     @@loop
+@@dec_height:
+    sub     r6, #01
+    ;mov     r0, r9
+    cmp     r6, #00
     beq     @@return
-    mov     r5, #1
+    mov     r5, r8
     b       @@loop
 @@return:
     add     sp, #8
+    pop     { r4-r5 }
+    mov     r8, r4
+    mov     r9, r5
+    mov     r10, r6
     pop     { r4-r7 }
     pop     { r0 }
     bx      r0
