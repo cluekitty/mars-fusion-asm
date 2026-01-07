e7056ad01ed80dce1fea390559d41cd56f844ee6
Miepee <janbidler00@protonmail.com>
2026-01-07 01:06:32 +0100

    Fix unable to go through drain pipe geron

--- src/nonlinear/nerf-geron-weakness.s
+++ src/nonlinear/nerf-geron-weakness.s
@@ -71,11 +71,62 @@
 .org 08029E68h
     mov r0, ClipdataAction_MakeSolid
 
-; Super Missile Geron
-.org 08041FA0h
-    mov r0, ClipdataAction_MakeSolid
-
 ; Power Bomb Geron
 .org 08042310h
     mov r0, ClipdataAction_MakeSolid
 
+
+
+; Make Super Missile Geron passable if it hasn't formed yet, but impoassable once it has formed.
+
+.definelabel @GeronSetCollision, 08029cf0h
+
+; Changes the function in SuperMissileGeronInit, to only set collision if pose is not Spawning_From_X
+.autoregion
+.align 2
+.func @SuperGeronInitHijack
+    push    { lr }
+    ldr     r1, =CurrentSprite
+    add     r1, Sprite_Pose
+    ldrb    r0, [r1, #0]
+    cmp     r0, #5Ah        ; SPRITE_POSE_SPAWNING_FROM_X
+    beq     @@return
+    mov     r0, ClipdataAction_MakeSolid
+    bl 	    @GeronSetCollision
+@@return:
+    pop     { pc }
+.endfunc
+.pool
+.endautoregion
+
+.org 08041FA0h      ; in SuperMissileGeronInit
+.area 6
+    bl @SuperGeronInitHijack
+    nop
+.endarea
+
+; Call GeronSetCollision at the end of SuperMissileGeronIdleInit
+.autoregion
+.align 2
+.func @SuperGeronIdleInitHijack
+    push    { lr }
+    mov     r0, ClipdataAction_MakeSolid
+    bl      @GeronSetCollision
+
+    ; Restore original code
+    add     r2, r1, #00
+    add     r2, #24h
+    mov     r3, #0
+    mov     r0, #2
+    strb    r0, [r2, #0]
+    pop     { pc }
+.endfunc
+.endautoregion
+
+.org 08041FB2h  ; end of SuperMissileGeronIdleInit
+.area 10
+    ; We need to manually save/restore lr here
+    push    { lr }
+    bl @SuperGeronIdleInitHijack
+    pop     { pc }
+.endarea
