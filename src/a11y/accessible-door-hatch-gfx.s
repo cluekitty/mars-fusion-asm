cebcbbe5e2310210b746616cf4e15a1c319ec544
clue cat <gitlab@clue.cat>
2026-02-09 23:40:58 -0500

    update comment for accessibility door graphics

--- src/a11y/accessible-door-hatch-gfx.s
+++ src/a11y/accessible-door-hatch-gfx.s
@@ -1,7 +1,7 @@
 ; Changes the graphics of the door hatches in order to
 ; make it possible to differentiate them without color.
 
-.org 083F28C8h  ; Address of common graphics
+.org 083F28C8h  ; Address of common background graphics
 .area 1000h
     .incbin "data/accessible-doors.gfx"
 .endarea
